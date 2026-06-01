0e9cd95abb6a85c793cee8a9892c91bd2c711a73
teamennovat <mashcatg@gmail.com>
2026-06-01 19:18:13 +0600

    asd

--- api/server-dist/assets/_tanstack-start-manifest_v-kbsWMq78.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/MSI/Desktop/australiya-sparkle-site/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-BDd22QBL.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-BDd22QBL.js" } }] }, "/": { filePath: "C:/Users/MSI/Desktop/australiya-sparkle-site/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-V4jf8FSe.js"] } } });
-export {
-  tsrStartManifest
-};
