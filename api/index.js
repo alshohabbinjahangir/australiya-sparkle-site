d3ba06a7235402e47393ae87d3ba145896fc4116
teamennovat <mashcatg@gmail.com>
2026-06-01 16:24:17 +0600

    asd

--- api/index.js
+++ api/index.js
@@ -1,9 +1,5 @@
 import server from '../dist/server/server.js';
 
-export const config = {
-  runtime: 'nodejs20.x',
-};
-
 export default async function handler(req, res) {
   const protocol = req.headers['x-forwarded-proto'] || 'https';
   const host = req.headers.host || 'localhost';
